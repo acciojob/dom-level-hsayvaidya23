453188816c14bac545065b41dc126ad93ebf3ed0
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-09-02 00:12:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const domLevel = (element) => {
+	const level = 0;
+	while(element.parentNode !== null) {
+		level++;
+		element = element.parentNode;
+	}
+	return level;
+}
+const target = document.getElementById('level');
+const Dlevel = domLevel(target);
+alert(`The level of the element is: ${Dlevel}`);
